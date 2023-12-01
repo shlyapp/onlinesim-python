520c7933f7e93a9258eb2381b22c675a0e3cc74e
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-12-01 08:39:46 +0400

    Init project

---
+++ start.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+os.chdir(parent_dir)
+
+scripts = [
+    'onlinesim-python.src.main',
+]
+
+for script in scripts:
+    subprocess.run(["python", "-m", script])
